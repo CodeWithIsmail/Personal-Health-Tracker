2e206ef3016095a91a9ff29a83ed92ee50aacdac
Mahfuz Ayon <134989125+Mahfuz-Ayon73@users.noreply.github.com>
2025-01-04 15:04:17 +0600

    Updated The report Filter UI a little bit

--- lib/components/ReportFilterWidget.dart
+++ lib/components/ReportFilterWidget.dart
@@ -11,10 +11,11 @@ class Reportfilterwidget extends StatefulWidget {
 
 class _ReportfilterwidgetState extends State<Reportfilterwidget> {
   DateTime? startDate;
-
   DateTime? endDate;
-
   final DateFormat dateFormat = DateFormat('dd-MMM-yyyy');
+
+  TextEditingController startDateController = TextEditingController();
+  TextEditingController endDateController = TextEditingController();
 
   void _showDateRangeDialog() {
     showDialog(
@@ -26,27 +27,31 @@ class _ReportfilterwidgetState extends State<Reportfilterwidget> {
         return AlertDialog(
           title: Text("Select Date Range"),
           content: Container(
-            width: 350, // Adjust the width as needed
-            height: 250, // Adjust the height as needed
+            width: 350,
+            height: 250,
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
+                SizedBox(height: 20,),
                 // Start Date TextField with Icon as the label
                 TextFormField(
                   readOnly: true,
-                  controller: TextEditingController(
-                      text: tempStartDate != null
-                          ? dateFormat.format(tempStartDate)
-                          : "Pick Start Date"),
+                  controller: startDateController
+                    ..text = tempStartDate != null
+                        ? dateFormat.format(tempStartDate)
+                        : "Start Date",
                   decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.calendar_today, color: Colors.black), // Icon at the start
-                    hintText: "Pick Start Date", // Placeholder text
-                    border: InputBorder.none, // Removes the default border
+                    prefixIcon: Icon(Icons.calendar_today,
+                        color: Colors.black),
+                    hintText: "Start Date",
+                    border: InputBorder.none,
                     enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black), // Bottom border
+                      borderSide:
+                          BorderSide(color: Colors.black),
                     ),
                     focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black), // Bottom border
+                      borderSide:
+                          BorderSide(color: Colors.black),
                     ),
                     contentPadding: EdgeInsets.all(10),
                   ),
@@ -60,27 +65,32 @@ class _ReportfilterwidgetState extends State<Reportfilterwidget> {
                     if (pickedStartDate != null) {
                       setState(() {
                         tempStartDate = pickedStartDate;
+                        startDateController.text =
+                            dateFormat.format(tempStartDate!); // Update text
                       });
                     }
                   },
                 ),
-                SizedBox(height: 10),
+                SizedBox(height: 20),
                 // End Date TextField with Icon as the label
                 TextFormField(
                   readOnly: true,
-                  controller: TextEditingController(
-                      text: tempEndDate != null
-                          ? dateFormat.format(tempEndDate)
-                          : "Pick End Date"),
+                  controller: endDateController
+                    ..text = tempEndDate != null
+                        ? dateFormat.format(tempEndDate)
+                        : "End Date",
                   decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.calendar_today, color: Colors.black), // Icon at the start
-                    hintText: "Pick End Date", // Placeholder text
+                    prefixIcon: Icon(Icons.calendar_today,
+                        color: Colors.black), // Icon at the start
+                    hintText: "End Date", // Placeholder text
                     border: InputBorder.none, // Removes the default border
                     enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black), // Bottom border
+                      borderSide:
+                          BorderSide(color: Colors.black), // Bottom border
                     ),
                     focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black), // Bottom border
+                      borderSide:
+                          BorderSide(color: Colors.black), // Bottom border
                     ),
                     contentPadding: EdgeInsets.all(10),
                   ),
@@ -94,6 +104,8 @@ class _ReportfilterwidgetState extends State<Reportfilterwidget> {
                     if (pickedEndDate != null) {
                       setState(() {
                         tempEndDate = pickedEndDate;
+                        endDateController.text =
+                            dateFormat.format(tempEndDate!); // Update text
                       });
                     }
                   },
@@ -104,28 +116,32 @@ class _ReportfilterwidgetState extends State<Reportfilterwidget> {
           actions: [
             // Cancel Button
             TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text("Cancel"),
-            ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("Cancel"),
+                style: TextButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    foregroundColor: Colors.white)),
+
             // Apply Button
             TextButton(
-              onPressed: () {
-                setState(() {
-                  startDate = tempStartDate;
-                  endDate = tempEndDate;
-                });
-                Navigator.pop(context);
-
-                // Optionally fetch data for the selected range here
-                if (startDate != null && endDate != null) {
-                  print("Selected Range: ${dateFormat.format(startDate!)} - ${dateFormat.format(endDate!)}");
-                  // Add your logic for filtering test values by date range
-                }
-              },
-              child: Text("Apply"),
-            ),
+                onPressed: () {
+                  setState(() {
+                    startDate = tempStartDate;
+                    endDate = tempEndDate;
+                  });
+                  Navigator.pop(context);
+                  if (startDate != null && endDate != null) {
+                    print(
+                        "Selected Range: ${dateFormat.format(startDate!)} - ${dateFormat.format(endDate!)}");
+                  }
+                },
+                child: Text("Apply"),
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.deepPurple,
+                  foregroundColor: Colors.white,
+                )),
           ],
         );
       },
@@ -134,46 +150,42 @@ class _ReportfilterwidgetState extends State<Reportfilterwidget> {
 
   @override
   Widget build(BuildContext context) {
-
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
     double containerWidth = screenWidth * 0.92; // 90% of screen width
     double containerHeight = screenHeight * 0.07; // 12% of screen height
 
     return GestureDetector(
-      onTap:  _showDateRangeDialog,
+      onTap: _showDateRangeDialog,
       child: Container(
-          height: containerHeight,
-          width: containerWidth,
-          margin: const EdgeInsets.only(bottom: 15),
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            color: Colors.indigo.shade300,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.2),
-                blurRadius: 5,
-                offset: Offset(0, 4),
+        height: containerHeight,
+        width: containerWidth,
+        margin: const EdgeInsets.only(bottom: 15),
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.indigo.shade300,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              blurRadius: 5,
+              offset: Offset(0, 4),
+            ),
+          ],
+        ),
+        child: Row(
+          children: [
+            Icon(Icons.filter_alt),
+            SizedBox(width: 20),
+            Text(
+              'Filter',
+              style: TextStyle(
+                fontSize: 20,
               ),
-            ],
-          ),
-          child: Row(
-            children: [
-              Icon(
-                Icons.filter_alt,
-              ),
-              SizedBox(
-                width: 20,
-              ),
-              Text(
-                'Filter',
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-              )
-            ],
-          )),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
