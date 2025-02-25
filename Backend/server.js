62d09470f87c4a6470d8b0622a05582c57e19a12
Mahfuz Ayon <134989125+Mahfuz-Ayon73@users.noreply.github.com>
2025-02-25 16:54:33 +0600

    Updated Server.js

---
+++ Backend/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv').config(); // Import dotenv at the very top
+
+const app = express();
+
+// Import routes
+const reportRoutes = require('./routes/report_routes'); // Adjust path as needed
+
+// Use body-parser middleware
+app.use(bodyParser.json());
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+// Use the routes
+app.use('/api', reportRoutes);  // Assuming your routes are under the "/api" prefix
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,'0.0.0.0', () => console.log('Server running on port ${PORT}'));
+
+app._router.stack.forEach((r) => {
+  if (r.route && r.route.path) {
+    console.log(r.route.path);
+  }
+}); 
