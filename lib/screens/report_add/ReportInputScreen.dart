5bb4d406a65b40196b787be8d311638493f0523d
CodeWithIsmail <hmismail360@gmail.com>
2025-03-09 12:41:19 +0600

    Update ReportInputScreen.dart

--- lib/screens/report_add/ReportInputScreen.dart
+++ lib/screens/report_add/ReportInputScreen.dart
@@ -52,48 +52,48 @@ class _ReportInputScreenState extends State<ReportInputScreen> {
       appBar: AppBar(title: Text("Report Input")),
       body: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: TextFormField(
-              controller: dayController,
-              textAlignVertical: TextAlignVertical.center,
-              readOnly: true,
-              onTap: () async {
-                DateTime? current = await showDatePicker(
-                  context: context,
-                  initialDate: selected,
-                  firstDate: DateTime.now().subtract(Duration(days: 36500)),
-                  lastDate: DateTime.now(),
-                );
-
-                if (current != null) {
-                  selected = current;
-                  // timestamp = Timestamp.fromDate(selected);
-                  dayController.text = DateFormat('dd-MMM-yy').format(current);
-                  //  print('Timestamp: $timestamp');
-                }
-              },
-              decoration: InputDecoration(
-                contentPadding:
-                    EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                hintText: 'Test date',
-                filled: true,
-                fillColor: Colors.white.withOpacity(0.8),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30),
-                  borderSide: BorderSide.none,
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30),
-                  borderSide: BorderSide(color: Colors.deepPurple, width: 0),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30),
-                  borderSide: BorderSide(color: Colors.purpleAccent, width: 1),
-                ),
-              ),
-            ),
-          ),
+          // Padding(
+          //   padding: const EdgeInsets.all(15.0),
+          //   child: TextFormField(
+          //     controller: dayController,
+          //     textAlignVertical: TextAlignVertical.center,
+          //     readOnly: true,
+          //     onTap: () async {
+          //       DateTime? current = await showDatePicker(
+          //         context: context,
+          //         initialDate: selected,
+          //         firstDate: DateTime.now().subtract(Duration(days: 36500)),
+          //         lastDate: DateTime.now(),
+          //       );
+          //
+          //       if (current != null) {
+          //         selected = current;
+          //         // timestamp = Timestamp.fromDate(selected);
+          //         dayController.text = DateFormat('dd-MMM-yy').format(current);
+          //         //  print('Timestamp: $timestamp');
+          //       }
+          //     },
+          //     decoration: InputDecoration(
+          //       contentPadding:
+          //           EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          //       hintText: 'Test date',
+          //       filled: true,
+          //       fillColor: Colors.white.withOpacity(0.8),
+          //       border: OutlineInputBorder(
+          //         borderRadius: BorderRadius.circular(30),
+          //         borderSide: BorderSide.none,
+          //       ),
+          //       enabledBorder: OutlineInputBorder(
+          //         borderRadius: BorderRadius.circular(30),
+          //         borderSide: BorderSide(color: Colors.deepPurple, width: 0),
+          //       ),
+          //       focusedBorder: OutlineInputBorder(
+          //         borderRadius: BorderRadius.circular(30),
+          //         borderSide: BorderSide(color: Colors.purpleAccent, width: 1),
+          //       ),
+          //     ),
+          //   ),
+          // ),
           Expanded(
             child: ListView(
               padding: EdgeInsets.all(16.0),
